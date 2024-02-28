9cedad29ca4022b66f9ea92bf5493feda29d5a64
QalibH <qalib.wot@gmail.com>
2024-02-28 15:18:41 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# Comment
+# print("helllo world")
+# print("Point : 7")
+# print("Point: ", 7, 15, ".", sep="|", end="\n")
+# print("second \" \t line")
+# print(5+5)
+# print(5-4)
+# print(5*5)
+# print(5/2)
+# print(5**3)
+# print(5//3)
+# print("point:", 5**5)
+# print(max(5, 6, 7, 8, 9, 10))
+# print(min(5, 6, 7, 8, 9, 10))
+# print(pow(5, 3))
+# print(abs(-5))
+# print(round(5/3))
+
+# number = 5
+
+# digit = 4.5989
+# word = "Point:"
+
+# print(word, digit)
+
+# number = 7
+
+# print("Point:", number)
+
+
+# num1 =int(input("enter the first number:"))
+
+# num2 =int(input("enter the second number:"))
+
+# print("Result:", num1 + num2)
+# print("Result:", num1 - num2)
+# print("Result:", num1 * num2)
+# print("Result:", num1 / num2)
+
+
+# user_data = int(input("enter the number: "))
+
+# if user_data > 5:
+#     print ("your nubmer bigger than 5")
+# if user_data == 5:
+#     print ("your nubmer equal to 5")
+# if user_data < 5:
+#     print ("your nubmer less than 5")
+
+
+
